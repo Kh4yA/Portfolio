701eb9c050dee3b56160cc230589020a6462f518
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2024-03-08 13:47:02 +0100

    portfolio depart
    1er jet d'une version du portfolio avec une page d'accueil

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnContact = document.getElementById('btn-contact')
+let modal = document.getElementById('modal')
+btnContact.addEventListener('click', (e)=>{
+    console.log(e);
+    modal.classList.add('active')
+})
+
