7fd4787319bf4dd1dad73474258ca572f87078a1
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2024-03-09 15:51:12 +0100

    Modification de la page d'accueil avec integration de ma photo et preparation de la version mobile

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let btnContact = document.getElementById('btn-contact')
-let modal = document.getElementById('modal')
-btnContact.addEventListener('click', (e)=>{
-    console.log(e);
-    modal.classList.add('active')
-})
+let openBurger = document.getElementById('open-burger')
+let burger = document.querySelector('.burger')
+let sommary = document.querySelector('.sommary')
 
 let acceuil = document.getElementById('acceuil')
 acceuil.addEventListener('mouseover', (e)=>{
 console.log(e);
+})
+openBurger.addEventListener('click', ()=>{
+    burger.classList.toggle('open')
+    sommary.classList.toggle('active')
+
 })
