e8535d7bc7177df03d2a101a85c1c443c6ddee12
Kh4yA <87660873+Kh4yA@users.noreply.github.com>
2024-03-15 16:25:45 +0100

    Mise en place des cartes effect lumineux

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,35 @@
+import Swiper from "../../node_modules/swiper/swiper-bundle.mjs"
+
 let openBurger = document.getElementById('open-burger')
 let burger = document.querySelector('.burger')
 let sommary = document.querySelector('.sommary')
 let btnNeon = document.getElementById('checkbox')
 let dev = document.querySelector('.dev')
-let acceuil = document.getElementById('acceuil')
-acceuil.addEventListener('mouseover', (e)=>{
-console.log(e);
-})
+let btnLegends = document.querySelectorAll('.btn')
+let competence = document.querySelector('.competence')
+let projet = document.querySelector('.projet')
+btnLegends.forEach(btn => {
+    btn.addEventListener('mouseover', ()=>{
+        btn.classList.add('bg-color')
+    })
+    btn.addEventListener('mouseout', ()=>{
+        btn.classList.remove('bg-color')
+    })
+});
 openBurger.addEventListener('click', ()=>{
     burger.classList.toggle('open')
     sommary.classList.toggle('active')
 
 })
 btnNeon.addEventListener('click',()=>{
-    console.log("ok");
-    dev.classList.toggle('neon-allume')
+    dev.classList.toggle('neon-allume-text')
+    competence.classList.toggle('neon-allume-box')
+    projet.classList.toggle('neon-allume-box')
 })
+
+const swiper = new Swiper('.swiper', {
+    effect: 'cards',
+    cardsEffect: {
+      // ...
+    },
+  });
